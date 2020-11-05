2766e0eabd3e8940daa6fd2c16b584c16be1af0f
bcturner777 <blaturne@cisco.com>
2020-11-05 15:07:03 -0800

    Add .gitignore and modify authentication prgram

--- Authentication.py
+++ Authentication.py
@@ -1,10 +1,16 @@
 from dnacentersdk import DNACenterAPI
+import os
 
 dnac_creds = {}
-dnac_creds['url'] = 'https://sandboxdnac2.cisco.com'
-dnac_creds['username'] = 'devnetuser'
-dnac_creds['password'] = 'Cisco123!'
+dnac_creds['url'] = os.getenv('URL')
+dnac_creds['username'] = os.getenv('USERNAME')
+dnac_creds['password'] = os.getenv('PASSWORD')
 
+print(dnac_creds)
 
 dnac = DNACenterAPI(username= dnac_creds['username'] , password= dnac_creds['password'], base_url=dnac_creds['url'])
-print("Auth Token: ", dnac.access_token)
+#print("Auth Token: ", dnac.access_token)
+
+devices = dnac.devices.get_device_list()
+for device in devices.response:
+    print("Hostname {} Device Management IP {}".format(device.hostname, device.managementIpAddress))
